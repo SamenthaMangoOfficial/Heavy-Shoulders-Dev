d0b388632f3a19d0a2496c13b5a581d84f6ca57e
SamenthaL <234454825+SamenthaMangoOfficial@users.noreply.github.com>
2025-11-20 20:39:08 -0500

    TestTextures

--- Heavy/Assets/Scripts/PlayerMovement.cs
+++ Heavy/Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,12 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] public InputActionAsset InputActions;
 
-    public float WalkSpeed = 5;
+    public float WalkSpeed = 2.5f;
+    public float SprintSpeed = 8;
     private InputAction m_moveAction;
+    private InputAction m_sprintAction;
     private Vector2 m_moveAmt;
+    private Vector2 m_sprintAmt;
     private Rigidbody2D m_rigidbody;
 
 
@@ -27,6 +30,8 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         m_moveAction = InputSystem.actions.FindAction("Player/Move");
+        m_sprintAction = InputSystem.actions.FindAction("Player/Sprint");
+
         //m_animator = GetComponent<Animator>();
         m_rigidbody = GetComponent<Rigidbody2D>();
     }
@@ -34,11 +39,13 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         m_moveAmt = m_moveAction.ReadValue<Vector2>();
+        m_sprintAmt = m_sprintAction.ReadValue<Vector2>();
     }
 
     private void FixedUpdate()
     {
         Walking();
+        //Sprinting();
     }
 
     private void Walking()
@@ -49,6 +56,12 @@ public class PlayerMovement : MonoBehaviour
     }
 
 
+    private void Sprinting()
+    {
+        //m_animator.SetFloat("Speed", m_moveAmt.y);
+        Vector2 newPos = m_rigidbody.position + m_moveAmt * SprintSpeed * Time.deltaTime;
+        m_rigidbody.MovePosition(newPos);
+    }
 
 
 }
